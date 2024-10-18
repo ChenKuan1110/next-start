3f3f25676da38192e231684bec2c824b985a1605
陈 宽 <18181347287@163.com>
2024-10-18 14:29:42 +0800

    change config
    修改 next 配置，验证部署在非主域名下

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export', // 指定静态导出
+  basePath: '/next-start',
+  assetPrefix: '/next-start',
 };
 
 export default nextConfig;
